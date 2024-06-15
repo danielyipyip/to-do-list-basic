c005fcdda4673f2e201751602a34ce56b564a53d
Daniel YIP <longyinyip@outlook.com>
2024-06-15 18:36:05 +0100

    baisc to do list copied from MDN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-list-basic';
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { name: "eat", done: true },
+    { name: "sleep", done: false },
+    { name: "play", done: false },
+    { name: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(name: string) {
+    if (!name) return;
+  
+    this.allItems.unshift({
+      name,
+      done: false
+    });
+  }
 }
